fd921438e5f80e4fd1056fdb37a9406f1e0da3cb
Seth Lawson <latesethlawsonhetchely@gmail.com>
2025-03-21 08:38:43 +0100

    ..

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module lawson.lonchi.crossword {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+
 
     opens lawson.lonchi.crossword to javafx.fxml;
     exports lawson.lonchi.crossword;
